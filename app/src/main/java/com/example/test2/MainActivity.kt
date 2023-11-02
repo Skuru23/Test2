26ed09f5dd716ab4a2bafc5fc3737b9eaf34a711
Skuru23 <98009221+Skuru23@users.noreply.github.com>
2023-11-02 23:09:58 +0700

    fix logo

--- app/src/main/java/com/example/test2/MainActivity.kt
+++ app/src/main/java/com/example/test2/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.test2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val logo:ImageView = findViewById(R.id.logo_image)
+        logo.setImageResource(R.drawable.kafka)
     }
 }
